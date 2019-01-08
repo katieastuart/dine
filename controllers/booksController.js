6d50c7931610d4c667ae70d7a7d7d3e30b005baf
Alias <37823208+MusicRocks@users.noreply.github.com>
2019-01-07 21:13:01 -0700

    removed key

--- controllers/booksController.js
+++ controllers/booksController.js
@@ -38,7 +38,7 @@ module.exports = {
   google: function(req, res) {
     axios({
       method: "GET",
-      url: `https://maps.googleapis.com/maps/api/place/textsearch/json?query=${req.body.foodType}&opennow&minprice=${req.body.minPrice}&maxprice=${req.body.maxPrice}&location=${req.body.latitude},${req.body.longitude}&radius=${req.body.distance}&key=AIzaSyAN-Maosba3R24Xqxv3aT-ZHcZ16dbzbdA`
+      url: `https://maps.googleapis.com/maps/api/place/textsearch/json?query=${req.body.foodType}&opennow&minprice=${req.body.minPrice}&maxprice=${req.body.maxPrice}&location=${req.body.latitude},${req.body.longitude}&radius=${req.body.distance}&key=`
     }).then(function (response) {
       res.json(response.data.results)
     })
