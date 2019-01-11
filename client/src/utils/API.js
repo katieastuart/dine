6da55aa93fd2b02f9da636fa663fb45a186ac4c8
Alias <37823208+MusicRocks@users.noreply.github.com>
2019-01-11 11:59:30 -0700

    updated some formatting

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -3,7 +3,6 @@ import axios from "axios";
 export default {
   // Get restaurant information
   google: function(userLocation) {
-    console.log("hit")
     return axios.post("/api/search/google", userLocation)
   }
 };
--- controllers/controller.js
+++ controllers/controller.js
@@ -5,20 +5,11 @@ const routes = require("../routes");
 // Defining methods for the booksController
 module.exports = {
   google: function(req, res) {
-    console.log("did this get hit");
-    console.log(req.body);
     axios({
       method: "GET",
-      url: `https://maps.googleapis.com/maps/api/place/textsearch/json?query=${
-        req.body.searchSelection.category
-      }&opennow&minprice=${req.body.minPrice}&maxprice=${
-        req.body.maxPrice
-      }&location=${req.body.latitude},${req.body.longitude}&radius=${
-        req.body.distance
-      }&key=`
+      url: `https://maps.googleapis.com/maps/api/place/textsearch/json?query=${req.body.searchSelection.category}&opennow&minprice=${req.body.minPrice}&maxprice=${req.body.maxPrice}&location=${req.body.latitude},${req.body.longitude}&radius=${req.body.distance}&key=`
     }).then(function(response) {
       res.json(response.data.results);
-      console.log(response.data.results);
     });
   }
   // ,
