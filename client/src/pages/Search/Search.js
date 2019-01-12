44c6bce1903bf463a443fbbc6980e4bffcb889a9
Gold Leader <whitneyb18@gmail.com>
2019-01-12 13:08:52 -0700

    Fixed 'treat' and 'food' buttons

--- client/src/pages/Search/Search.js
+++ client/src/pages/Search/Search.js
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
 import { Container } from "../../components/Grid";
 import API from "../../utils/API";
-import { Card, CardImg, CardBody, Button } from 'reactstrap';
+import { Card, CardImg, CardBody, Button } from "reactstrap";
 import Modal from "../../components/Modal";
 import treatSearch from "./treatSearch.json";
-import foodSearch from "./foodSearch.json"
-import { Col, Form, FormGroup, Label, Input } from 'reactstrap';
-import {Redirect} from 'react-router-dom';
+import foodSearch from "./foodSearch.json";
+import { Col, Form, FormGroup, Label, Input } from "reactstrap";
+import { Redirect } from "react-router-dom";
 
 class Search extends Component {
   state = {
@@ -28,28 +28,26 @@ class Search extends Component {
   };
 
   componentDidMount = () => {
-    var storeLocation = (userLocationInformation) => {
-        var userLatitude = userLocationInformation.coords.latitude;
-        var userLongitude = userLocationInformation.coords.longitude;
-        this.setState({latitude: userLatitude, longitude: userLongitude }) 
-      
-    }
+    var storeLocation = userLocationInformation => {
+      var userLatitude = userLocationInformation.coords.latitude;
+      var userLongitude = userLocationInformation.coords.longitude;
+      this.setState({ latitude: userLatitude, longitude: userLongitude });
+    };
 
-    var errors = (errorInformation) => {
+    var errors = errorInformation => {
       if (errorInformation) {
-          alert("please allow location information to use website")
+        alert("please allow location information to use website");
       }
-    }
+    };
 
     if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(storeLocation, errors)
+      navigator.geolocation.getCurrentPosition(storeLocation, errors);
     } else {
-        alert("Geolocation is not supported by this browser")
+      alert("Geolocation is not supported by this browser");
     }
-    
-  } 
+  };
 
-  handleInputChange = (event) => {
+  handleInputChange = event => {
     let value = event.target.value;
     const name = event.target.name;
 
@@ -58,30 +56,31 @@ class Search extends Component {
     });
   };
 
-  setFood = (event) => {
+  setFood = event => {
     event.preventDefault();
 
     this.setState({
       searchSelection: foodSearch[this.state.index]
-    })
-  }
+    });
 
-  setTreat = (event) => {
+    this.handleFormSubmit();
+  };
+  handleFormSubmit = event => {
+    // Preventing the default behavior of the form submit (which is to refresh the page)
+
+    this.setState({
+      initialQuestions: true
+    });
+  };
+
+  setTreat = event => {
     event.preventDefault();
 
     this.setState({
       searchSelection: treatSearch[this.state.index]
-    })
-  }
-
-  handleFormSubmit = (event) => {
-    // Preventing the default behavior of the form submit (which is to refresh the page)
-    event.preventDefault();
-
-    this.setState({
-      initialQuestions: true
-    })
-  }
+    });
+    this.handleFormSubmit();
+  };
 
   incrementIndex = () => {
     if (this.state.index === 5) {
@@ -90,7 +89,7 @@ class Search extends Component {
       });
     }
 
-    if (this.state.searchSelection === treatSearch[this.state.index] ) {
+    if (this.state.searchSelection === treatSearch[this.state.index]) {
       this.setState({
         index: this.state.index + 1,
         searchSelection: treatSearch[this.state.index + 1]
@@ -101,94 +100,117 @@ class Search extends Component {
         searchSelection: foodSearch[this.state.index + 1]
       });
     }
-  }
- 
+  };
+
   googleAPICall = () => {
-    console.log(this.state.searchSelection)
-    API.google(this.state).then(res => console.log(res.data))
-    
-      if (this.state.index === 5) {
+    console.log(this.state.searchSelection);
+    API.google(this.state).then(res => console.log(res.data));
+
+    if (this.state.index === 5) {
+      this.setState({
+        redirect: true
+      });
+    }
+
+    if (this.state.index < 5) {
+      if (this.state.searchSelection === treatSearch[this.state.index]) {
         this.setState({
-          redirect: true
+          index: this.state.index + 1,
+          searchSelection: treatSearch[this.state.index + 1]
+        });
+      } else {
+        this.setState({
+          index: this.state.index + 1,
+          searchSelection: foodSearch[this.state.index + 1]
         });
       }
-
-      if (this.state.index < 5) {
-        if (this.state.searchSelection === treatSearch[this.state.index] ) {
-          this.setState({
-            index: this.state.index + 1,
-            searchSelection: treatSearch[this.state.index + 1]
-          });
-        } else {
-          this.setState({
-            index: this.state.index + 1,
-            searchSelection: foodSearch[this.state.index + 1]
-          });
-        }
-      }
     }
-  
-    render() {
-      if(!this.state.initialQuestions) {
-        return (
-          <Container>
-            <Form>
+  };
 
-              <FormGroup row>
-                <Label sm={2}>set distance 1000-50000</Label>
-                <Col sm={10}>
-                  <Input onChange={this.handleInputChange} value={this.state.distance} name="distance" type="text" />
-                </Col>
-              </FormGroup>
-
-              <FormGroup row>
-                <Label sm={2}>set minPrice 0-4</Label>
-                <Col sm={10}>
-                  <Input onChange={this.handleInputChange} value={this.state.minPrice} name="minPrice" type="text" />
-                </Col>
-              </FormGroup>
-
-              <FormGroup row>
-                <Label sm={2}>set maxPrice 0-4</Label>
-                <Col sm={10}>
-                  <Input onChange={this.handleInputChange} value={this.state.maxPrice} name="maxPrice" type="text" />
-                </Col>
-              </FormGroup>
-
-              <FormGroup row>
-                <Col sm={10}>
-                <p>Please select treat or food</p>
-                <Button onClick={this.setTreat}>treat</Button>
-                <Button onClick={this.setFood}>food</Button>
-                </Col>
-              </FormGroup>
-             
-              <Button onClick={this.handleFormSubmit}>submit</Button>
-            </Form>
-          </Container>
-        );
-      }
-
-      if(this.state.redirect){
-        return <Redirect to="/results"/>
-      }
-      
+  render() {
+    if (!this.state.initialQuestions) {
       return (
         <Container>
-          <Modal />
+          <Form>
+            <FormGroup row>
+              <Label sm={2}>Distance</Label>
+              <Col sm={10}>
+                <Input
+                  onChange={this.handleInputChange}
+                  value={this.state.distance}
+                  name="distance"
+                  type="text"
+                />
+              </Col>
+            </FormGroup>
 
-            {
-                <Card>
-                  <h1>{this.state.searchSelection.name}</h1>
-                  <CardImg top width="100%" src={this.state.searchSelection.image} alt="Card image cap" />
-                
-                  <CardBody>
-                    <Button onClick={this.googleAPICall}>Yes</Button>
-                    <Button onClick={this.incrementIndex}>No</Button>
-                  </CardBody>
-                </Card>
-            }
-     
+            <FormGroup row>
+              <Label sm={2}>Min Price</Label>
+              <Col sm={10}>
+                <Input
+                  onChange={this.handleInputChange}
+                  value={this.state.minPrice}
+                  name="minPrice"
+                  type="text"
+                />
+              </Col>
+            </FormGroup>
+
+            <FormGroup row>
+              <Label sm={2}>Max Price</Label>
+              <Col sm={10}>
+                <Input
+                  onChange={this.handleInputChange}
+                  value={this.state.maxPrice}
+                  name="maxPrice"
+                  type="text"
+                />
+              </Col>
+            </FormGroup>
+
+            <FormGroup row>
+              <Col sm={10}>
+                <p>Please select treat or food</p>
+                <Button onClick={this.setTreat}>
+                  treat
+                  {/* <img src="../../images/ice-cream.png" /> */}
+                </Button>
+                <Button onClick={this.setFood}>food</Button>
+              </Col>
+            </FormGroup>
+          </Form>
+        </Container>
+      );
+    }
+
+    if (this.state.redirect) {
+      return <Redirect to="/results" />;
+    }
+
+    return (
+      <Container>
+        <Modal />
+
+        {
+          <Card>
+            <h1 style={{ background: "pink" }} draggable="true">
+              {this.state.searchSelection.name}
+            </h1>
+            <CardImg
+              className="foodCard"
+              top
+              width="100%"
+              src={this.state.searchSelection.image}
+              alt="Card image cap"
+              draggable="true"
+            />
+
+            <CardBody>
+              <Button onClick={this.googleAPICall}>Yes</Button>
+              <Button onClick={this.incrementIndex}>No</Button>
+            </CardBody>
+          </Card>
+        }
       </Container>
     );
   }
