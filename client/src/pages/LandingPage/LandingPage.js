2c9a14d99c8c8b41cf6cc66ce04d05f509cb8add
Gold Leader <whitneyb18@gmail.com>
2019-01-22 18:51:14 -0700

    Profile styling, adding launch page

--- client/src/pages/LandingPage/LandingPage.js
+++ client/src/pages/LandingPage/LandingPage.js
@@ -1,9 +1,11 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 import { Container } from "../../components/Grid";
-// import { Media, Button } from 'reactstrap';
+import { Row, Col } from 'reactstrap';
 import { Redirect } from 'react-router-dom';
 import { MyContext } from '../../App';
 import Nav from "../../components/Nav";
+import "./LandingPage.css"
 
 
 class LandingPage extends Component {
@@ -20,7 +22,26 @@ class LandingPage extends Component {
                         <div>
                             <Nav />
                             <Container>
-                                <h2>This is the Landing page</h2>
+                                <Row className="landingRow">
+                                    <Col md={4} className="landingPage">
+                                        <Link to="/search">
+                                            <div className="profileImgSelect">
+                                                <img className="card-img-top" src="https://images.vexels.com/media/users/3/132068/isolated/preview/f9bb81e576c1a361c61a8c08945b2c48-search-icon-by-vexels.png" alt="search"></img>
+                                                <div className="centered imgText">New Search</div>
+                                            </div>
+                                        </Link>
+                                    </Col>
+                                    <Col md={4} className="landingPage">
+                                        <Link to="/results">
+                                            Results
+                                        </Link>
+                                    </Col>
+                                    <Col md={4} className="landingPage">
+                                        <Link to="/favorites">
+                                            Favorites
+                                        </Link>
+                                    </Col>
+                                </Row>
                             </Container>
                         </div>
                     )
